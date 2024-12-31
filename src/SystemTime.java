2b63115eb2a9974655d4ef7e73112eba50af9b9c
riskgame15 <tttungcr@gmail.com>
2024-12-31 11:06:39 +0700

    java system time

---
+++ src/SystemTime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date(); // lay thoi gian hien tai
+        System.out.println("Thoi gian hien tai la: " + now);
+
+    }
+}
